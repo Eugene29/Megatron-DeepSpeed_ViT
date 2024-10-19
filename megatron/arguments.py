enabled Ring-Attention, positional encoding, faster Ulysses with USP, enabled packed Ulysses from USP, added correct tflops formula for VIT

megatron/arguments.py
@@ -50,8 +50,8 @@ def parse_args(extra_args_provider=None, ignore_unknown_args=False):
     # Custom arguments.
     if extra_args_provider is not None:
         parser = extra_args_provider(parser)
-    parser.add_argument("--use_unifiedSP", action="store_true")
-    parser.add_argument("--use_wandb", action="store_true")
+    # parser.add_argument("--use_unifiedSP", action="store_true")
+    # parser.add_argument("--use_wandb", action="store_true")
 
     parser = deepspeed.add_config_arguments(parser)
 
@@ -71,7 +71,12 @@ def parse_args(extra_args_provider=None, ignore_unknown_args=False):
     # Args from environment
     args.rank = int(os.getenv('RANK', '0'))
     args.world_size = int(os.getenv("WORLD_SIZE", '1'))
-    # args.local_rank = torch.cuda.current_device()
+    args.pos_encoding = os.getenv('POS_ENCODING', 0)
+    ## TODO: unify below under one string variable
+    args.USP_ulysses = os.getenv('USP_ulysses', 0)
+    args.USP_ring = os.getenv('USP_ring', 0)
+    args.USP_hybrid = os.getenv('USP_hybrid', 0)
+    args.use_unifiedSP = args.USP_ulysses or args.USP_ring or args.USP_hybrid
 
     return args
 

megatron/data/image_folder.py
@@ -216,28 +216,35 @@ class DatasetFolder(VisionDataset):
             tuple: (sample, target) where target is class_index of the target class.
         """
         curr_index = index
+        from megatron import get_args
+        args = get_args()
         for x in range(self.total):
             try:
-
-                # from megatron import get_args
-                # args = get_args()
-                # with open(os.environ["DATA_PATH_LOG"], "a") as file:
-                #     # traceback.print_stack(file=file)
-                #     # rank_str = f"Rank is {args.rank} for below image data \n"
-                #     rank_str = ""
-                #     file.write(rank_str + path + '\n')
-
-                #### Using Toy Dataset ####
-                if os.environ["DATA"] != "Toy":
-                    path, target = self.samples[curr_index]
-                    sample = self.loader(path)
-                else:
+                if os.environ["DATA"] == "Toy":
+                    #### Toy Dataset ####
                     import torch
                     assert "IMG_W" in os.environ
                     w = int(os.environ["IMG_W"])
                     h = int(os.environ["IMG_H"])
-                    sample = torch.randn(3, w, h, dtype=torch.float16) ## Doesn't let 92 channels
+                    c = args.num_channels
+                    sample = torch.randn(c, w, h, dtype=torch.float16)
                     target = torch.randint(10, ())
+
+                    if "DATA_PATH_LOG" in os.environ:
+                        with open(os.environ["DATA_PATH_LOG"], "a") as file:
+                            file.write("#" * 30 + "\n")
+                            file.write(sample + '\n')
+                            file.write(target + '\n')
+                            file.flush()
+
+                else:
+                    path, target = self.samples[curr_index]
+                    sample = self.loader(path)
+
+                    if "DATA_PATH_LOG" in os.environ:
+                        with open(os.environ["DATA_PATH_LOG"], "a") as file:
+                            file.write(path + '\n')
+                            file.flush()
                 break
             except Exception as e:
                 curr_index = np.random.randint(0, self.total)

megatron/model/vision/vit_backbone.py
@@ -200,17 +200,42 @@ class VitBackbone(MegatronModule):
             )
 
             # embedding
-            self.position_embeddings = torch.nn.Embedding(
-                self.seq_length, self.hidden_size
-            )
-            init_method_normal(args.init_method_std)(
-                self.position_embeddings.weight
-            )
+            self.pos_encoding = args.pos_encoding ## TODO: pass it as init argument or as config
+            if self.pos_encoding:
+                ## TODO: Understanding the theroy behind pos encoding and optimize it for longer sequences. 
+                def positionalencoding1d(d_model, length):
+                    """
+                    :param d_model: dimension of the model
+                    :param length: length of positions
+                    :return: length*d_model position matrix
+                    """
+                    if d_model % 2 != 0:
+                        raise ValueError("Cannot use sin/cos positional encoding with "
+                                        "odd dim (got dim={:d})".format(d_model))
+                    dtype = torch.float16
+                    pe = torch.zeros((length, d_model), dtype=dtype)
+                    position = torch.arange(0, length, dtype=dtype).unsqueeze(1)
+                    div_term = torch.exp((torch.arange(0, d_model, 2, dtype=dtype) *
+                                        -(math.log(10000.0) / d_model)))
+                    pe[:, 0::2] = torch.sin(position.float() * div_term)
+                    pe[:, 1::2] = torch.cos(position.float() * div_term)
+                    return pe
+                
+                self.position_embeddings = positionalencoding1d(self.hidden_size, self.seq_length).to(args.rank)
+            else:
+                self.position_embeddings = torch.nn.Embedding(
+                    self.seq_length, self.hidden_size
+                )
 
             args.class_token_present = self.class_token
-            self.position_embeddings._register_load_state_dict_pre_hook(
-                twod_interpolate_position_embeddings_hook
-            )
+            
+            if not self.pos_encoding:
+                init_method_normal(args.init_method_std)(
+                    self.position_embeddings.weight
+                )
+                self.position_embeddings._register_load_state_dict_pre_hook(
+                    twod_interpolate_position_embeddings_hook
+                )
 
             self.embedding_dropout = torch.nn.Dropout(args.hidden_dropout)
             # # Dropout.
@@ -254,8 +279,15 @@ class VitBackbone(MegatronModule):
                 cls_tokens = self.cls_token.expand(encoder_output.shape[0], -1, -1)
                 concatenated_tokens = torch.cat((cls_tokens, encoder_output), dim=1)
 
-            token_embeddings = concatenated_tokens + \
-                    self.position_embeddings(self.position_ids[:, :concatenated_tokens.shape[1]])
+            if self.pos_encoding:
+                # print(f"self.position_embeddings: {self.position_embeddings.shape}")
+                # print(f"concatenated_tokens: {concatenated_tokens.shape}")
+                token_embeddings = concatenated_tokens + self.position_embeddings
+
+            else:
+                token_embeddings = concatenated_tokens + \
+                        self.position_embeddings(self.position_ids[:, :concatenated_tokens.shape[1]])
+
             # [b, s, h] => [s, b, h]
             # token_embeddings = token_embeddings.transpose(0, 1).contiguous()
             hidden_states = token_embeddings.transpose(0, 1).contiguous()

megatron/training.py
@@ -1330,12 +1330,29 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
         log_dir = "./trace_vit/"
         os.makedirs(log_dir, exist_ok=True)
         rank = torch.cuda.current_device()
-        Ulysses = "unifiedSP" if "unifiedSP" in os.environ else "DS"
         WS = torch.distributed.get_world_size()
         FA = 'FA_' if 'FA' in os.environ else ""
         SP = "SP" + os.environ["SP"] + "_"
+        DATA = os.environ["DATA"]
+        if "USP_ulysses" in os.environ:
+            framework = "USP_ulysses"
+        elif "USP_ring" in os.environ:
+            framework = "USP_ring"
+        elif "USP_hybrid" in os.environ:
+            framework = "USP_hybrid"
+        else:
+            framework = "DS"
+        
+        if "PACKED" in os.environ:
+            if os.environ["PACKED"] == "5D":
+                packed = "packed5D_"
+            else:
+                packed = "packed_"
+        else:
+            packed = ""
+
         if torch.distributed.get_rank() == 0:
-            p.export_chrome_trace(log_dir + f"WS{WS}_{Ulysses}_{FA}{SP}rank{rank}.json")
+            p.export_chrome_trace(log_dir + f"{DATA}_WS{WS}_{framework}_{packed}{FA}{SP}rank{rank}.json")
 
     import os
     profile_enabled = "PROFILE" in os.environ
@@ -1352,7 +1369,7 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
         p.start()
         args.train_iters = 10
 
-    def num_floating_point_operations(args, batch_size):
+    def llm_num_floating_point_operations(args, batch_size):
         # Group Query Attention.
         # if not args.group_query_attention:
         if not args.num_key_value_heads:
@@ -1381,14 +1398,37 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
                 + (args.padded_vocab_size / (2 * args.num_layers * args.hidden_size))
             )
         )
+
+    def num_floating_point_operations(args, batch_size):
+        B = batch_size ## Global Batch Size
+        s = args.seq_length ## sequence length
+        h = args.hidden_size ## emb hidden dimension
+        h_ = args.ffn_hidden_size ## ffnn hidden dimension
+        l = args.num_layers ## num att. layer
+        p = args.patch_dim ## patch size
+        c = args.num_channels ## num channels
+
+        Att_computation = 4 * B * s**2 * h ## QK^T, (NxN) @ V
+        Lin_Transform = 8 * B * s * h**2 ## Q, K, V, O
+        MLP = 4 * B * s * h * h_ ## 2 Lin Transform
+        Lin_Embedding = 2 * B * s * p**2 * c * h ## (B s p^2*c) @ (p^2*c h)
+        ## Since num_classes << s * h, we can ignore num_classes.
+
+        return 3 * (l * (Att_computation +  Lin_Transform + MLP) + Lin_Embedding) ## x3 for fwd + bwd(2x)
+
+    ## Checkout Model parameters
     args = get_args()
-    from time import time
+    from torchinfo import summary
+    summary(model[0])
+    model[0]
+    # raise KeyboardInterrupt("break")
+
     # from deepspeed.profiling.flops_profiler import FlopsProfiler
     # prof = FlopsProfiler(model[0])
     # prof.start_profile()
     while iteration < args.train_iters and (args.train_tokens is None or \
         args.consumed_train_tokens < args.train_tokens):
-        strt = time()
+        strt = time.time()
         trigger(on_step_begin)
         update_num_microbatches(args.consumed_train_samples)
         if args.deepspeed:
@@ -1398,6 +1438,7 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
                                 get_num_microbatches()
             model[0].set_train_batch_size(global_batch_size)
             tot_Tflops = num_floating_point_operations(args=args, batch_size=global_batch_size) / 1000 ** 4
+            llm_tot_Tflops = llm_num_floating_point_operations(args=args, batch_size=global_batch_size) / 1000**4
             # print(f"Megatron-LM formula? tot_Tflops: {tot_Tflops}")
 
         ## Check all code updates and clean then up. 
@@ -1409,6 +1450,14 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
                     update_rotary_pos_emb(curriculum_seqlen)
             args.curriculum_seqlen = curriculum_seqlen
         args.curr_iteration = iteration
+        if "DATA_PATH_LOG" in os.environ:
+            args = get_args()
+            if args.rank == 0:
+                with open(os.environ["DATA_PATH_LOG"], "a") as file:
+                    file.write("\n" + "#"*30 + f" iter {iteration} " + f"#"*30 + "\n")
+                    file.flush()
+            torch.distributed.barrier()
+
         loss_dict, skipped_iter, grad_norm, num_zeros_in_grad = \
             train_step(forward_step_func,
                     train_data_iterator,
@@ -1417,7 +1466,6 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
                     opt_param_scheduler,
                     config)
         
-        step_time1 = time() - strt
         # model_agnostic_DS_flop_count = prof.get_total_flops()
         # if torch.distributed.get_rank() == 0:
         #     print(f"DS tflop count: {model_agnostic_DS_flop_count // 10**12}")
@@ -1535,9 +1583,15 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
         if profile_enabled:
             p.step()
 
-        step_time2 = time() - strt
+        step_time = time.time() - strt
         max_memory_used = torch.cuda.max_memory_allocated() / (1024**3)
-        print_rank_0(f"iteration: {iteration} time: {step_time1, step_time2} TFLOPS: {tot_Tflops / step_time2} Max_memory: {max_memory_used}")
+        samples_per_sec = global_batch_size / step_time
+        print_rank_0(f"iteration:{iteration} \t"
+                        f"time:{step_time:.2f} \t"
+                        f"LLM_TFLOPS:{llm_tot_Tflops / step_time:.2f} \t"
+                        f"TFLOPS:{tot_Tflops / step_time:.2f} \t"
+                        f"Samples/Sec:{samples_per_sec:.2f} \t"
+                        f"Max_memory:{max_memory_used:.2f}" )
 
     if profile_enabled:
         p.stop()